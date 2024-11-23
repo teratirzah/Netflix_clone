4806a8d1061b14159f08ead8cd9224a3ab3603de
akilas-solo <tersameshesha@gmail.com>
2024-11-22 22:45:23 -0800

    header , axios and request

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+// import Footer from "../../Components/Footer/Footer";
+// import Row from "../../Components/RowLists/Row";
+const Home = () => {
+  return (
+    <div>
+     
+       <Header />
+      {/* <Row />
+      <Footer />  */}
+    </div>
+  );
+}
+
+export default Home;
